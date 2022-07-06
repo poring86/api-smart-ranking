2442825260ffce7cfde7fe729199f91e53721a77
Matheus Lino <matheuslino86a@gmail.com>
2022-07-05 20:12:19 -0400

    get jogadores e atualizar por email

--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -8,8 +8,29 @@ export class JogadoresService {
   private jogadores: Jogador[] = [];
   private readonly logger = new Logger(JogadoresService.name);
 
-  async criarAtualizarJogador(CriarJogadorDto: CriarJogadorDto): Promise<void> {
-    await this.criar(CriarJogadorDto);
+  async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
+    const { email } = criarJogadorDto;
+
+    const jogadorEncontrado = await this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+
+    if (jogadorEncontrado) {
+      await this.atualizar(jogadorEncontrado, criarJogadorDto);
+    } else {
+      await this.criar(criarJogadorDto);
+    }
+  }
+  private atualizar(
+    jogadorEncontrado: Jogador,
+    criarJogadorDto: CriarJogadorDto,
+  ): void {
+    const { nome } = criarJogadorDto;
+    jogadorEncontrado.nome = nome;
+  }
+
+  async consultarTodosJogadores(): Promise<Jogador[]> {
+    return await this.jogadores;
   }
 
   private criar(criaJogadorDto: CriarJogadorDto): void {
