0466a103a2ecd49005ed6a1474c068776f754e61
Matheus Lino <matheuslino86a@gmail.com>
2022-07-24 18:19:34 -0400

    fix type

--- src/desafios/desafios.controller.ts
+++ src/desafios/desafios.controller.ts
@@ -1,12 +1,14 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { DesafiosService } from './desafios.service';
 import { CriarDesafioDto } from './dtos/criar-desafio.dto';
+import { Desafio } from './interfaces/desafio.interface';
 
 @Controller('api/v1/desafios')
 export class DesafiosController {
@@ -14,7 +16,9 @@ export class DesafiosController {
 
   @Post()
   @UsePipes(ValidationPipe)
-  async criarDesafio(@Body() CriarDesafioDto: CriarDesafioDto): Promise<any> {
+  async criarDesafio(
+    @Body() CriarDesafioDto: CriarDesafioDto,
+  ): Promise<Desafio> {
     return await this.desafiosService.criarDesafio(CriarDesafioDto);
   }
 }
